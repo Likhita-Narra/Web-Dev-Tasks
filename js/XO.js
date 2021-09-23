abb41a73637a273caa4906a33c45e0d228034929
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-09-23 11:32:32 +0530

    Created hover effect

---
+++ js/XO.js
@@ -0,0 +1,11 @@
+var v1, v2, v3, v4, v5, v6, v7, v8, v9;
+
+v1 = document.getElementById("v1").innerHTML;
+v2 = document.getElementById("v2").innerHTML;
+v3 = document.getElementById("v3").innerHTML;
+v4 = document.getElementById("v4").innerHTML;
+v5 = document.getElementById("v5").innerHTML;
+v6 = document.getElementById("v6").innerHTML;
+v7 = document.getElementById("v7").innerHTML;
+v8 = document.getElementById("v8").innerHTML;
+v9 = document.getElementById("v9").innerHTML;
