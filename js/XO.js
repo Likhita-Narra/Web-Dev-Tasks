df1ed2839d7408aac6c4b1382bf9d6973b77e62e
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-09-23 14:33:22 +0530

    Converted paragraphs to form elements

--- js/XO.js
+++ js/XO.js
@@ -9,3 +9,19 @@ v6 = document.getElementById("v6").innerHTML;
 v7 = document.getElementById("v7").innerHTML;
 v8 = document.getElementById("v8").innerHTML;
 v9 = document.getElementById("v9").innerHTML;
+
+function highlight(element) {
+    if (element.value == null) {
+        var flag = "X";
+        element.value = flag;
+    }
+
+    if (flag == "X") {
+        flag == "O";
+    }
+
+    else if (flag == "O") {
+        flag = "X";
+    }
+    element.style.color = "white";
+}
