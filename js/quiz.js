68b54a577e4dd3d91e6b986c55ab99aaa42d7201
Likhita-Narra <narra.likhita2020@vitstudent.ac.in>
2021-12-11 10:05:29 +0530

    First commit

---
+++ js/quiz.js
@@ -0,0 +1,5 @@
+document.getElementById("question").innerHTML = "In web development, what does CSS stand for?";
+document.getElementById("option1").innerHTML = "Counter Strike: Source";
+document.getElementById("option2").innerHTML = "Corrective Style Sheet";
+document.getElementById("option3").innerHTML = "Computer Style Sheet";
+document.getElementById("option4").innerHTML = "Cascading Style Sheet";
